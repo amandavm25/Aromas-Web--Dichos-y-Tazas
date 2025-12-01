cb7e47064ef554a80452d574ef8ab512b448410d
Evelyn-20 <evelynmasisg05@gmail.com>
2025-11-30 20:28:12 -0600

    Modificaciones listados

--- AromasWeb/AromasWeb/obj/Debug/net8.0/AromasWeb.AssemblyInfo.cs
+++ AromasWeb/AromasWeb/obj/Debug/net8.0/AromasWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AromasWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a09da8b358f94befe006c02ebda77964f86f16e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c524eeb40091b98bcd3394bb0d1130f5da0a42f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AromasWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AromasWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
