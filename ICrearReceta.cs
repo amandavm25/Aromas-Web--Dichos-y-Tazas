eab11c23ae83992d17a68d4d43da3f6099f1076e
estefanleon <136112490+estefanleon@users.noreply.github.com>
2026-02-02 01:04:57 -0500

    Add files via upload
    REC-01/REC-02

---
+++ ICrearReceta.cs
@@ -0,0 +1,30 @@
+﻿//using AromasWeb.Abstracciones.ModeloUI;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AromasWeb.Abstracciones.Logica.Receta
+//{
+//    public interface ICrearReceta
+//    {
+//        int Crear(Receta receta);
+//    }
+//}
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecetaUI = AromasWeb.Abstracciones.ModeloUI.Receta;
+
+namespace AromasWeb.Abstracciones.Logica.Receta
+{
+    public interface ICrearReceta
+    {
+        Task<int> Crear(RecetaUI laReceta);
+    }
+}
