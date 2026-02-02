8c09fd8f7e4a9b3f6d79a80115d45c825fa75103
estefanleon <136112490+estefanleon@users.noreply.github.com>
2026-02-02 16:25:17 -0500

    REC-01/REC-02

---
+++ AromasWeb/AromasWeb.LogicDeNegocio/Receta/EliminarReceta.cs
@@ -0,0 +1,33 @@
+﻿using AromasWeb.LogicDeNegocio.Receta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AromasWeb.LogicDeNegocio.Receta
+{
+    internal class EliminarReceta
+    {
+    }
+}
+
+
+namespace AromasWeb.LogicaDeNegocio.Receta
+{
+    public class EliminarReceta : Abstracciones.Logica.Receta.IEliminarReceta
+    {
+        private IEliminarReceta _eliminarReceta;
+
+        public EliminarReceta()
+        {
+            _eliminarReceta = new AccesoADatos.Receta.EliminarReceta();
+        }
+
+        public int Eliminar(int id)
+        {
+            int resultado = _eliminarReceta.Eliminar(id);
+            return resultado;
+        }
+    }
+}
